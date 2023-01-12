50403327ed99307b132b214540e0ad915501b607
dvoraen <42825596+dvoraen@users.noreply.github.com>
2023-01-12 04:28:53 -0800

    add: admin interface

--- world/settings/admin.py
+++ world/settings/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from world.settings.models import (
+    PlayerSettings,
+    CommSettings,
+    GeneralSettings,
+    RPSettings,
+)
+
+
+class GeneralSettingsInline(admin.StackedInline):
+    model = GeneralSettings
+    can_delete = False
+
+
+class CommSettingsInline(admin.StackedInline):
+    model = CommSettings
+    can_delete = False
+
+
+class RPSettingsInline(admin.StackedInline):
+    model = RPSettings
+    can_delete = False
+
+
+class PlayerSettingsAdmin(admin.ModelAdmin):
+    # readonly_fields = ("character",)
+    inlines = [GeneralSettingsInline, CommSettingsInline, RPSettingsInline]
+
+
 # Register your models here.
+admin.site.register(PlayerSettings, PlayerSettingsAdmin)
+admin.site.register(GeneralSettings)
+admin.site.register(CommSettings)
+admin.site.register(RPSettings)
